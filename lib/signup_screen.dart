a1d289170b9fe669ea471388901428e09778d6e5
Rowida Hussein <Rowida.Hussein.22230416@commerce.helwan.edu.eg>
2024-09-11 11:23:40 +0200

    SCRUM-23  git commit -m "SCRUM-23 <Partially Done>"

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,186 +1,190 @@
-  import 'package:flutter/material.dart';
-  import 'package:intl/intl.dart'; // Ensure you have this package in your pubspec.yaml
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Ensure you have this package in your pubspec.yaml
+import 'login_screen.dart'; // Import the LoginPage
 
-  class SignupScreen extends StatelessWidget {
-    const SignupScreen({super.key});
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: SignUpScreen(),
-        debugShowCheckedModeBanner: false,
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignUpScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  DateTime? selectedDate;
+  final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
+  final TextEditingController _dateController = TextEditingController();
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
   }
 
-  class SignUpScreen extends StatefulWidget {
-    @override
-    _SignUpScreenState createState() => _SignUpScreenState();
-  }
-
-  class _SignUpScreenState extends State<SignUpScreen> {
-    DateTime? selectedDate;
-    final DateFormat dateFormat = DateFormat('dd/MM/yyyy');
-    final TextEditingController _dateController = TextEditingController();
-
-    @override
-    void dispose() {
-      _dateController.dispose();
-      super.dispose();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: const Color(0xFF6936F5), // Background color
-        body: SafeArea(
-          child: Column(
-            children: [
-              // Header with back arrow and "Sign up" text
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back, color: Colors.white),
-                      onPressed: () {
-                        Navigator.pop(context); // Navigate back to the previous screen
-                      },
-                    ),
-                    const SizedBox(width: 10),
-                    const Text(
-                      'Sign up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Center(
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    margin: const EdgeInsets.symmetric(horizontal: 16),
-                    decoration: BoxDecoration(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF6936F5), // Background color
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Header with back arrow and "Sign up" text
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.white),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()), // Navigate to LoginPage
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Sign up',
+                    style: TextStyle(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(16),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Name',
-                              border: OutlineInputBorder(),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Phone number',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextField(
+                          readOnly: true, // Read-only to ensure selection from the date picker
+                          decoration: const InputDecoration(
+                            labelText: 'DD/MM/YYYY',
+                            border: OutlineInputBorder(),
+                          ),
+                          controller: _dateController,
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: selectedDate ?? DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100),
+                            );
+                            if (pickedDate != null && pickedDate != selectedDate) {
+                              setState(() {
+                                selectedDate = pickedDate;
+                                _dateController.text = dateFormat.format(selectedDate!);
+                              });
+                            }
+                          },
+                        ),
+                        const SizedBox(height: 16),
+                        DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Select gender',
+                          ),
+                          items: ['Male', 'Female'].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {},
+                        ),
+                        const SizedBox(height: 16),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Emergency contact',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(),
+                          ),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 16),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Confirm password',
+                            border: OutlineInputBorder(),
+                          ),
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 32),
+                        // Sign up button
+                        ElevatedButton(
+                          onPressed: () {
+                            // Add sign-up logic
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(200, 50),
+                            backgroundColor: const Color(0xFF6936F5),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
                             ),
                           ),
-                          const SizedBox(height: 16),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              border: OutlineInputBorder(),
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
                             ),
                           ),
-                          const SizedBox(height: 16),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Phone number',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          TextField(
-                            readOnly: true, // Read-only to ensure selection from the date picker
-                            decoration: const InputDecoration(
-                              labelText: 'DD/MM/YYYY',
-                              border: OutlineInputBorder(),
-                            ),
-                            controller: _dateController,
-                            onTap: () async {
-                              DateTime? pickedDate = await showDatePicker(
-                                context: context,
-                                initialDate: selectedDate ?? DateTime.now(),
-                                firstDate: DateTime(1900),
-                                lastDate: DateTime(2100),
-                              );
-                              if (pickedDate != null && pickedDate != selectedDate) {
-                                setState(() {
-                                  selectedDate = pickedDate;
-                                  _dateController.text = dateFormat.format(selectedDate!);
-                                });
-                              }
-                            },
-                          ),
-                          const SizedBox(height: 16),
-                          DropdownButtonFormField<String>(
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Select gender',
-                            ),
-                            items: ['Male', 'Female'].map((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            onChanged: (newValue) {},
-                          ),
-                          const SizedBox(height: 16),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Emergency contact',
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(),
-                            ),
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 16),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: 'Confirm password',
-                              border: OutlineInputBorder(),
-                            ),
-                            obscureText: true,
-                          ),
-                          const SizedBox(height: 32),
-                          // Sign up button
-                          ElevatedButton(
-                            onPressed: () {
-                              // Add sign-up logic
-                            },
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: const Size(200, 50),
-                              backgroundColor: const Color(0xFF6936F5),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                            child: const Text(
-                              'Sign up',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
+}
