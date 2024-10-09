20d43cb8d546676feb846063db36da9327196c90
Mohamed Sayed <mohameddsayedd17@gmail.com>
2024-10-10 01:06:06 +0300

    Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eldercare/Screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; // Import your generated Firebase options
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false, // Hide the debug banner
-      home: SplashScreen(), // Set the SplashScreen as the home screen
+      home: ProfileScreen(), // Set the SplashScreen as the home screen
     );
   }
 }
+// 17
