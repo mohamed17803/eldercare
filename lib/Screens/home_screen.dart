979c96fbefecc6bbb8b270988389af3d596af372
Mohamed Sayed <mohameddsayedd17@gmail.com>
2024-10-15 05:59:31 +0300

    SCRUM-45 git commit -m "SCRUM-45 <Final Touches>"

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,13 +1,13 @@
 import 'package:eldercare/Screens/profile_screen.dart';
-import 'package:eldercare/Screens/progress_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 import 'add_edit_med_screen.dart';
 import 'login_screen.dart';
 import 'medicationdet_screen.dart';
 import 'setting_screen.dart';
-import 'history_screen.dart'; // Ensure you import your history screen here
+import 'history_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,8 +20,8 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   static const List<Widget> screens = <Widget>[
-    HomeScreenContent(),  // Ensure HomeScreenContent is included here
-    HistoryScreen(),  // Make sure this points to the correct HistoryScreen widget
+    HomeScreenContent(),
+    HistoryScreen(),
     EditScreen(),
     SettingsPage(),
   ];
@@ -35,15 +35,18 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF6936F5),
       appBar: AppBar(
         title: const Text(
-          'Today\'s plan',
+          'Today\'s Plan',
           style: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.bold,
+            fontFamily: 'Pacifico',
+            color: Colors.white,
           ),
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: const Color(0xFF6936F5),
       ),
       drawer: Drawer(
         child: ListView(
@@ -51,45 +54,53 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             const DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Color(0xFF6936F5),
               ),
               child: Text(
-                'Eldercare Menu',
+                'Elderly Care',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
+                  fontFamily: 'Pacifico',
                 ),
               ),
             ),
             ListTile(
               leading: const Icon(Icons.person),
-              title: const Text('Profile'),
+              title: const Text(
+                'Profile',
+                style: TextStyle(fontFamily: 'Pacifico'),
+              ),
               onTap: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (context) => ProfileScreen(),
+                    builder: (context) => const ProfileScreen(),
                   ),
                 );
-                // Add navigation to Profile screen or any action
               },
             ),
             ListTile(
               leading: const Icon(Icons.notifications),
-              title: const Text('Notifications'),
-              onTap: () {
-                // Add navigation to Notifications screen or any action
-              },
+              title: const Text(
+                'Notifications',
+                style: TextStyle(fontFamily: 'Pacifico'),
+              ),
+              onTap: () {},
             ),
             ListTile(
               leading: const Icon(Icons.help),
-              title: const Text('Help'),
-              onTap: () {
-                // Add navigation to Help screen or any action
-              },
+              title: const Text(
+                'Help',
+                style: TextStyle(fontFamily: 'Pacifico'),
+              ),
+              onTap: () {},
             ),
             ListTile(
               leading: const Icon(Icons.logout),
-              title: const Text('Logout'),
+              title: const Text(
+                'Logout',
+                style: TextStyle(fontFamily: 'Pacifico'),
+              ),
               onTap: () {
                 FirebaseAuth.instance.signOut();
                 Navigator.of(context).pushReplacement(
@@ -111,7 +122,7 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
+        selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.fixed,
         onTap: _onItemtap,
@@ -127,12 +138,11 @@ class HomeScreenContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final currentUser = FirebaseAuth.instance.currentUser;
 
-    // Check if currentUser is null, meaning no user is logged in
     if (currentUser == null) {
       return const Center(
         child: Text(
           'User not logged in',
-          style: TextStyle(fontSize: 18),
+          style: TextStyle(fontSize: 18, fontFamily: 'Pacifico'),
         ),
       );
     }
@@ -144,7 +154,7 @@ class HomeScreenContent extends StatelessWidget {
           .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return const Center(child: Text('Something went wrong'));
+          return const Center(child: Text('Something went wrong', style: TextStyle(fontFamily: 'Pacifico')));
         }
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
@@ -153,19 +163,39 @@ class HomeScreenContent extends StatelessWidget {
         final medications = snapshot.data!.docs;
 
         if (medications.isEmpty) {
-          return const Center(child: Text('No medications added yet.'));
+          return const Center(child: Text('No medications added yet.', style: TextStyle(fontFamily: 'Pacifico')));
         }
+
+        final today = DateTime.now();
+        final String todayDate = DateFormat('yyyy-MM-dd').format(today);
 
         return ListView.builder(
           itemCount: medications.length,
           padding: const EdgeInsets.all(16),
           itemBuilder: (context, index) {
             var medication = medications[index];
+            var medicationData = medication.data() as Map<String, dynamic>;
+
+            // Extract alarm trigger times for today
+            List<dynamic> schedule = medicationData['schedule'] ?? [];
+            String alarmTime = '';
+            for (var entry in schedule) {
+              if (entry['alarm_trigger_time'] != null &&
+                  entry['alarm_trigger_time'] is Timestamp) {
+                DateTime alarmTriggerTime = (entry['alarm_trigger_time'] as Timestamp).toDate();
+                if (DateFormat('yyyy-MM-dd').format(alarmTriggerTime) == todayDate) {
+                  alarmTime = DateFormat.jm().format(alarmTriggerTime); // Format the time as needed
+                  break; // Use the first matching alarm time
+                }
+              }
+            }
+
             return MedicationCard(
               medicationId: medication.id,
-              medicationName: medication['medication_name'],
-              dosageValue: medication['dosage']['value'].toString(),
-              dosageUnit: medication['dosage']['unit'],
+              medicationName: medicationData['medicine'] ?? 'Unknown Medication',
+              dosageValue: (medicationData['dosage']?['value']?.toString() ?? '0'),
+              dosageUnit: (medicationData['dosage']?['unit'] ?? 'N/A'),
+              alarmTime: alarmTime, // Pass the alarm time to MedicationCard
             );
           },
         );
@@ -174,19 +204,20 @@ class HomeScreenContent extends StatelessWidget {
   }
 }
 
-
 class MedicationCard extends StatelessWidget {
-  final String medicationId; // Added medication ID
+  final String medicationId;
   final String medicationName;
   final String dosageValue;
   final String dosageUnit;
+  final String alarmTime; // Add alarm time
 
   const MedicationCard({
     Key? key,
-    required this.medicationId, // Accept medication ID
+    required this.medicationId,
     required this.medicationName,
     required this.dosageValue,
     required this.dosageUnit,
+    required this.alarmTime, // Accept alarm time
   }) : super(key: key);
 
   @override
@@ -198,19 +229,28 @@ class MedicationCard extends StatelessWidget {
         contentPadding: const EdgeInsets.all(16),
         title: Text(
           medicationName,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18, fontFamily: 'Pacifico'),
         ),
-        subtitle: Text('Dosage: $dosageValue $dosageUnit'),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Dosage: $dosageValue $dosageUnit', style: const TextStyle(fontFamily: 'Pacifico')),
+            Text('Alarm Time: $alarmTime', style: const TextStyle(fontFamily: 'Pacifico')), // Display the alarm time
+          ],
+        ),
         trailing: ElevatedButton(
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => MedicationDetailsScreen(medicationId: medicationId), // Pass medication ID
+                builder: (context) => MedicationDetailsScreen(medicationId: medicationId),
               ),
             );
           },
-          child: const Text('Details'),
+          child: const Text(
+            'Details',
+            style: TextStyle(fontFamily: 'Pacifico'),
+          ),
         ),
       ),
     );
